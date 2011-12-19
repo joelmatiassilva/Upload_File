4b5defd3d8874d00ea99a424f86b1995109738da
Joel Silva <joelsilva_87@hotmail.com>
2011-12-19 00:08:05 -0300

    se toma el nombre del archivo

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,43 +1,35 @@
 require 'docsplit'
 
 class Upload < ActiveRecord::Base
-  attr_accessible :detalle, :resume
+  attr_accessible :resume, :ruta
   mount_uploader :resume, ResumeUploader
   before_save :crear_archivo_text
-  after_save :guardar_contenido_text_en_tabla
+  before_create :guardar_contenido_text_en_tabla
+  after_save :despues_de_salvar
+
+  def despues_de_salvar
+   	debugger	
+  end
 
   def crear_archivo_text
-   cad = "" 
-   self.detalle = ""
    Docsplit.extract_text("/home/joel/Escritorio/Upload_File/public"+resume.to_s, :output=>'public/archivos')
-  # debugger
-    #File.open("/home/joel/Escritorio/Upload_File/public"+resume.to_s.gsub(".pdf",".txt"), 'r') do |f1|
-      #debugger
-
-     # while linea = f1.gets
-    #	cad = cad + linea
-    # end
-    #end
-    #debugger
-   # self.detalle = cad
-    #debugger
-    #File.open('joe.txt', 'w') do |f2|
-     #f2.puts cad
-    #end
-  end
+debugger
+end
 
   def guardar_contenido_text_en_tabla
 	cadena = ""
-	self.detalle =""
-    	File.open("/home/joel/Escritorio/Upload_File/public"+resume.to_s.gsub(".pdf", ".txt"), 'r') do |f1|
+        self.detalle =""
+	debugger
+    	File.open("/home/joel/Escritorio/Upload_File/public/archivos/"+resume.file.filename.to_s.gsub(".pdf", ".txt"), 'r') do |f1|
    		while lineaArchivo = f1.gets
 	 	 cadena = cadena + lineaArchivo
  		end
 	end
-	debugger
 	self.detalle = cadena
 	debugger
   end
- 
+
+#  def guardar_contenido_text_en_tabla
+ # end 
 
 end
